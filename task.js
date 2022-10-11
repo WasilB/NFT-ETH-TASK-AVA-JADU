e58f4140511def0f8b781a5126fda03e7b8ef491
WasilB <muhammadwasil23703@gmail.com>
2022-10-11 19:01:36 +0500

    jsonFile

--- task.js
+++ task.js
@@ -2,9 +2,12 @@ const Web3 = require("web3");
 const abi_def = require("./abi");
 const ava_abi = require("./avaAbi");
 const abi = abi_def;
+let test;
 
 const rpcURL = "https://mainnet.infura.io/v3/21d1df8208a048dfa2fa3acf5ba3776b";
 const web3 = new Web3(rpcURL);
+var data = [];
+const accountAddr = "0xD4f0053c4e9c599a719b36dc20d5dEd1198230af";
 const ContractAddress = [
   { abi: abi, Address: "0xeDa3b617646B5fc8C9c696e0356390128cE900F8" },
   { abi: abi, Address: "0xd0F0C40FCD1598721567F140eBf8aF436e7b97cF" },
@@ -17,15 +20,32 @@ async function getdata() {
       ContractAddress[i].abi,
       ContractAddress[i].Address
     );
-    await contract.methods.name().call((err, result) => {
-      console.log(result, "name");
-    });
-    await contract.methods
-      .balanceOf("0xD4f0053c4e9c599a719b36dc20d5dEd1198230af")
-      .call((err, result) => {
-        console.log(result, "Balance");
-      });
+
+    if (i == 2) {
+      const name = await contract.methods.name().call();
+      const tokens = await contract.methods.tokensOfOwner(accountAddr).call();
+      const balance = await contract.methods.balanceOf(accountAddr).call();
+
+      data.push({ name: name, Balance: balance, TokensID: tokens });
+    } else {
+      const stoken = await contract.methods.balanceOf(accountAddr).call();
+      for (let i = 0; i < stoken; i++) {
+        const result = await contract.methods
+          .tokenOfOwnerByIndex(accountAddr, i)
+          .call();
+        test = result;
+      }
+
+      const name = await contract.methods.name().call();
+      const balance = await contract.methods.balanceOf(accountAddr).call();
+      data.push({ name: name, Balance: balance, TokensID: test });
+    }
   }
 }
+async function print() {
+  await getdata();
 
-getdata();
+  console.log(data);
+}
+
+print();
